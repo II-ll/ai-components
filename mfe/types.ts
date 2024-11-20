a30151615a21379176c47f5e15faad64358ccf5b
Drew H <dhardie@clearblade.com>
2024-11-20 15:47:37 -0600

    minor mfe update

--- mfe/types.ts
+++ mfe/types.ts
@@ -1,17 +1,15 @@
-import { AssetType } from "@clearblade/ia-mfe-core";
-
-export type AiComponentsProps = {
+export type ComponentsProps = {
   schema: Record<string, unknown>[];
   component: {
     id: string;
     name: string;
-    meta: Record<string, unknown>;
+    settings: Record<string, unknown>;
   }
   assetTypeName: string;
   setValues: React.Dispatch<
     React.SetStateAction<{
       schema: Record<string, unknown>[];
-      componentsMeta?: Record<string, unknown>;
+      settings?: Record<string, unknown>;
     }>
   >
 };
