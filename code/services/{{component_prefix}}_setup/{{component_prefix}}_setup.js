298c30be70a55e643f359371328e9ce0f46fca5a
Drew H <dhardie@clearblade.com>
2024-11-22 14:13:49 -0600

    updated JSDOC for component handler services

--- code/services/{{component_prefix}}_setup/{{component_prefix}}_setup.js
+++ code/services/{{component_prefix}}_setup/{{component_prefix}}_setup.js
@@ -1,17 +1,7 @@
 /**
- * Type: Micro Service
- * Description: A short-lived service which is expected to complete within a fixed period of time.
- * @param {CbServer.BasicReq} req
- * @param {string} req.systemKey
- * @param {string} req.systemSecret
- * @param {string} req.userEmail
- * @param {string} req.userid
- * @param {string} req.userToken
- * @param {boolean} req.isLogging
- * @param {[id: string]} req.params
+ * @typedef {{prefix: string, entity_id: string, component_id: string, mfe_settings: Record<string, unknown>}} InstallParams
+ * @param {CbServer.BasicReq & {params: InstallParams}} req
  * @param {CbServer.Resp} resp
- *
- * @typedef {{prefix: string, entity_id: string, component_id: string, mfe_settings: Record<string, unknown>}} setupParams
  */
 
 function {{component_prefix}}_setup(req, resp) {
